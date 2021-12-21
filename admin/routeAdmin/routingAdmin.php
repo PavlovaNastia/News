0998afcc70b452c6fddfb2029aaff25fb4116715
PavlovaNastia <78953875+PavlovaNastia@users.noreply.github.com>
2021-12-21 17:23:44 +0200

    Редактирование новостей

--- admin/routeAdmin/routingAdmin.php
+++ admin/routeAdmin/routingAdmin.php
@@ -1,37 +1,38 @@
 <?php
+//вычислить маршрут из адресной строки
 $host = explode('?', $_SERVER['REQUEST_URI'])[0];
-$num=substr_count($host, '/');
+$num = substr_count($host, '/');
 $path = explode('/', $host)[$num];
-
-if ($path == '' OR $path == 'index.php' )
-{
-    // Главная страница - 
+if ($path == "" OR $path == "index") {
+    //Главная страница входа
     $response = controllerAdmin::formLoginSite();
-}
-//------Вход------------------------------------------
-elseif ($path == 'login')
-{
-    // Форма входа
+} elseIf ($path == 'login') {
+    //форма входа
     $response = controllerAdmin::loginAction();
-}
-elseif ($path == 'logout')
-{
-    // Выход
+} elseIf ($path == 'logout') {
+    //Выход
     $response = controllerAdmin::logoutAction();
 }
-//-------------------------------------------listNews
-elseif($path=='newsAdmin'){
+    //-------------------------------listNews
+elseif($path=='newsAdmin') {
     $response=controllerAdminNews::NewsList();
 }
-//-------------------------------add news
-    elseif($path=='newsAdd'){
+//-----------------add news
+    elseif($path=='newsAdd') {
         $response=controllerAdminNews::newsAddForm();
-
-}    
-    elseif($path == 'newsAddResult') {
+}
+elseif($path == 'newsAddResult') {
     $response = controllerAdminNews::newsAddResult();
 }
-else
-{  // Страница не существует
+
+//--------------------------------edit news
+elseif ($path=='newsEdit' && isset($_GET['id'])){
+    $response=controllerAdminNews::newsEditForm($_GET['id']);
+}
+elseif ($path == 'newsEditResult' && isset($_GET['id'])) {
+    $response=controllerAdminNews::newsEditResult($_GET['id']);
+}
+else {
+    // страница не существует
     $response = controllerAdmin::error404();
-} 
+}
