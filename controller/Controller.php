46a5618048c3ec0c35373b760a2544459aa03d95
PavlovaNastia <78953875+PavlovaNastia@users.noreply.github.com>
2021-12-21 16:33:26 +0200

    добавлена форма регистрации

--- controller/Controller.php
+++ controller/Controller.php
@@ -55,7 +55,16 @@ class Controller {
         ViewComments::CommentsCountWithAncor($arr);
     }
 
+    //--------------------------------------------РЕГИСТРАЦИЯ
+    public static function registerForm() {
+        include_once('view/formRegister.php');
+    }
 
+    public static function registerUser() {
+        $result = Register::registerUser();
+
+        include_once('view/answerRegister.php');
+    }
 
 }// end class 
 
--- view/layout.php
+++ view/layout.php
@@ -1,30 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>NEWSPORTAL</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif" rel="stylesheet">
-    <meta charset="utf-8">
-</head>
-<body>
-    <nav class="one">
-        <ul class="topmenu">
-            <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i></a>
+    <head>
+        <title>NEWSPORTAL</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel= "stylesheet" type ="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        <meta charset="utf-8">
+    
+    </head>
+    <body>
+    
+<nav class="one">
+    <ul class="topmenu">
+    <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i></a>
         <ul class="submenu">
-            <?php
-            Controller::AllCategory();
-            ?>
-            </ul>
+        <?php
+              Controller::AllCategory();
+        ?>
+      </ul>
+    </li>
+    <li><a href="testError">Info</a></li>
+    <li><a href="iwww">Info</a></li>
+    <li><a href="./">Stardileht</a></li>
+    <li><a href="registerForm">Register </a></li>
+    <div class="pull-right">
+        <li>
+            <form action="search">
+                <input type="text" name="otsi">
+                <input type="submit" value="Otsi">
+            </form>
         </li>
-        <li><a href="testError">Info</a></li>
-        <li><a href="./">Stardileht</a></li>
+    </div>  
 
-</ul>
+    </ul>
 </nav>
-
 <section>
-    <div class="divBox">
+    <div class = 'divBox'>
         <?php
         if(isset($content)) {
             echo $content;
@@ -33,10 +44,9 @@
             echo '<h1>Content is gone!</h1>';
         }
         ?>
+    </div>
 </section>
-
-<hr>
-<p style="display:block; text-align:center;">SKTVp20 2021 a. &copy</p>
-</body>
-
+        <hr>
+        <p style="display:block; text-align:center;">SKTVp20 2021 &copy</p>
+    </body>
 </html>
